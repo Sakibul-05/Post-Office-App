88a086a97f111ec3e984df78ed8a0cf57faafda6
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-08-06 13:02:23 +0530

    Update location.js

--- location.js
+++ location.js
@@ -1,4 +1,3 @@
-
 const ip = document.getElementById('ip');
 const latitude = document.getElementById('latitude')
 const longtitude = document.getElementById('longtitude')
@@ -14,128 +13,56 @@ const numberOfPincode = document.getElementById('numberOfPincode');
 const postOffices = document.querySelector(".post-offices");
 const searchInput = document.getElementById('searchInput');
 
-
-// function myFunction(){
-//     console.log('Page loaded');
-//     console.log(JSON.parse(sessionStorage.getItem('locationInfo')));
-//    //fetch ip address 
-//    fetch('https://api.ipify.org?format=json')
-//   .then(response => response.json())
-//   .then((data)=>{
-//     console.log("id data: ",data);
-  
-//     //After fetching ip address then this getLocationInfo function called
-//     getLocationInfo(data.ip) 
-//   });
-// // }
-
-
-// function getLocationInfo(ip){
-//   let url = `http://ip-api.com/json/${ip}`;
-//   // const url = `https://api.ipapi.is/?q=${ip}`;
-//   //console.log(`url: ${url}`);
-//   fetch(url).then(res=> res.json()).then ((data)=> {
-//     console.log(data);
-     
-//     latitude.innerText = data.lat;
-//     iframe.src = `https://maps.google.com/maps?q=${data.lat}, ${data.lon}&z=15&output=embed`
-     
-//     console.log(data.lat);
-//     longtitude.innerText = data.lon;
-//     console.log(data.lon);
-//     city.innerText = data.city;
-//     region.innerText = data.region;
-//     organisation.innerText = data.org;
-//     timeZone.innerText = data.timezone;
-//     // datetime based on timezone
-//     let  datetime_str = new Date().toLocaleString("en-US", { timeZone: `${data.timezone}` });
-
-//     // "3/22/2021, 5:05:51 PM"
-//     console.log(datetime_str);
-//     dateTime.innerText =  datetime_str;
-//     pincode.innerText = data.zip;
-//     getPostOffice(data.zip)
-//   })
-    
-// }
-
-
-// function getPostOffice(pin){
-//   const url = `https://api.postalpincode.in/pincode/${pin}`
-//   fetch(url).then(res=> res.json()).then(data=>{
-//     console.log(data);
-//     const postOfficeArray = data[0].PostOffice;
-//     numberOfPincode.innerText = postOfficeArray.length
-//     displayPostOffices(postOfficeArray);
-//     searchInput.addEventListener('input', (event)=>{
-//       event.preventDefault()
-//       const searchTerm = searchInput.value.toLowerCase();
-//       //filtering postoffices by using name
-//       console.log(searchTerm);
-//       // console.log(postOfficeArray);
-//       const filteredPostOffices = postOfficeArray.filter((element)=>{
-//         return element.Name.toLowerCase().includes(searchTerm) 
-//       })
-//       console.log(filteredPostOffices);
-//       displayPostOffices(filteredPostOffices);
-//     })
-     
-//   })
-// }
-
-// function displayPostOffices(postofficesArray){
-//   postOffices.innerHTML = "";
-//   postofficesArray.forEach(element => {
-//     const htmlCode = `<div class="postOffice">
-//                         <p>Name: <span>${element.Name}</span></p>
-//                         <p>Branch Type: <span>${element.BranchType}</span></p>
-//                         <p>Delivery Status: <span>${element.DeliveryStatus}</span></p>
-//                         <p>District: <span>${element.District}</span></p>
-//                         <p>Division: <span>${element.Division}</span></p>
-//                       </div>`;
-//     postOffices.innerHTML += htmlCode;                  
-//   });
-// } 
-
  
-
 
 //get data from seasonStorage
 //parse that data
-const dataArray = JSON.parse(sessionStorage.getItem("dataArray"))
- 
-if(Array.isArray(dataArray)){
+const ipInfo = JSON.parse(sessionStorage.getItem("ipInfo"))
+console.log(ipInfo);
+// {
+//   city: "Kolkata",
+//   country: "IN",
+//   ip: "103.155.194.48",
+//   loc: "22.5626,88.3630",
+//   org: "AS140151 Ethereal Business Solutions Private Limited",
+//   postal: "700006",
+//   readme: "https://ipinfo.io/missingauth",
+//   region: "West Bengal",
+//   timezone: "Asia/Kolkata"
+// }
+
+
+if(ipInfo){
   
-      //1st one stored into locationInfo
-      let locationInfo = dataArray[0];
-      //2nd one stored into ipAddress
-      let ipAddress = dataArray[1];
+     const lat = ipInfo.loc.split(',')[0]
+     const lon = ipInfo.loc.split(',')[1]
       
 
       //show ip address in UI
-      ip.innerText = ipAddress;
+      ip.innerText = ipInfo.ip;
 
         
 
-      latitude.innerText = locationInfo.lat;
-      longtitude.innerText = locationInfo.lon;
-      city.innerText = locationInfo.city;
-      region.innerText = locationInfo.region;
-      organisation.innerText = locationInfo.org;
-      timeZone.innerText = locationInfo.timezone;
-      iframe.src = `https://maps.google.com/maps?q=${locationInfo.lat}, ${locationInfo.lon}&z=15&output=embed`
+      latitude.innerText =  lat;
+      longtitude.innerText =  lon;
+      city.innerText = ipInfo.city;
+      region.innerText = ipInfo.region;
+      organisation.innerText = ipInfo.org;
+      hostname.innerText = ipInfo.readme;
+      timeZone.innerText = ipInfo.timezone;
+      iframe.src = `https://maps.google.com/maps?q=${lat}, ${lon}&z=15&output=embed`
       
       // Use the timezone from the JSON response to get the time of the user's location
       // datetime based on timezone
-      let  datetime_str = new Date().toLocaleString("en-US", { timeZone: `${locationInfo.timezone}` });
-
-      // "3/22/2021, 5:05:51 PM"
+      let  datetime_str = new Date().toLocaleString("en-US", { timeZone: `${ipInfo.timezone}` });
+      
+      // "8/5/2022, 5:05:51 PM"
       console.log(datetime_str);
       dateTime.innerText =  datetime_str;
-      pincode.innerText = locationInfo.zip;
+      pincode.innerText = ipInfo.postal;
 
       //call this function --> as the function is regular function so i can call it before declearation
-      getPostOffice(locationInfo.zip)
+      getPostOffice(ipInfo.postal)
 }else{
   console.log("Unable to get the data");
   alert(`Unable to get the data from server`);
@@ -170,9 +97,15 @@ function getPostOffice(pin){
     })
      
   })
+  .catch(error=>{
+    console.log(error);
+    alert("https://api.postalpincode.in/pincode/${pin} --> this api doesn't work");
+  })
 }
 
+//display the UI for each postoffice
 function displayPostOffices(postofficesArray){
+  //every 1st time calling UI should empty
   postOffices.innerHTML = "";
   postofficesArray.forEach(element => {
     const htmlCode = `<div class="postOffice">
@@ -182,8 +115,8 @@ function displayPostOffices(postofficesArray){
                         <p>District: <span>${element.District}</span></p>
                         <p>Division: <span>${element.Division}</span></p>
                       </div>`;
+    //append to the UI                  
     postOffices.innerHTML += htmlCode;                  
   });
 } 
-
  
